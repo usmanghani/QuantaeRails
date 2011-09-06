c8fb08818c97fbc048e06cee1b16c844f1e4564b
Usman Ghani <usman.ghani@gmail.com>
2011-09-05 21:11:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuantaeRails::Application.config.secret_token = '5597128aef5d1a3c1b53c1c57c3eee4ffb8495c317f6e23310dbe04a962dff78d83ed45ecf7c49fe9114fc1902d9c534ec293c0c60e4b1c821338362012c7e7f'
